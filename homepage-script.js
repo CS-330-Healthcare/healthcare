80994f59209e8c3ea3eb2ec4e57d1ec50adb3a79
Ethan1101315 <44911286+Ethan1101315@users.noreply.github.com>
2020-03-09 18:27:35 -0500

    added a function for toggling the dropdown

--- homepage-script.js
+++ homepage-script.js
@@ -5,3 +5,12 @@ function goToNewPage()
         window.location = url;
     }
 }
+
+function toggleShowHide() { // reveal the dropdown only after "Find a service" is clicked.
+  var x = document.getElementById("dropdown");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
